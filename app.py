f647d956afd6dbb7f529070316ae973414b1e9d3
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-18 17:38:48 -0800

    Update secret key

--- app.py
+++ app.py
@@ -40,6 +40,6 @@ def submit():
     return redirect(url_for('index'), code=307)
 
 if __name__ == '__main__':
-    app.secret_key = 'secret'
+    app.secret_key = os.urandom(24)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
