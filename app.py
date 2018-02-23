914b71d96b5e3edf8337bed3f2e049a6ba431334
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-22 17:57:28 -0800

    Fix #6

--- app.py
+++ app.py
@@ -17,27 +17,58 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    """
+    Handles Coconut code submitted by users.
+    """
     code = request.form['code']
 
+    # Write a code to a file with randomly generated filename.
     filename = str(uuid.uuid4())
     with open(filename, 'w') as output:
         output.write(code)
 
-    subprocess.run(["coconut", filename], check=True)
-    print("Finish compilation [{:}] to [{:}.py]".format(filename, filename))
+    # Initialize parameters
+    outputText = None
+    compileError = None
+    runningError = None
+    proc = None
 
-    proc = subprocess.run(["python", filename + ".py"], stdout=subprocess.PIPE, check=True)
-    print("Finish running [{:}]".format(filename + ".py"))
+    # Compile the user's code with Coconut compiler
+    try:
+        subprocess.run(["coconut", filename], check=True)
+    except subprocess.CalledProcessError as error:
+        compileError = True
+        outputText = "Compile Error"
+        print("Error in compiling Coconut's code")
 
-    outputText = proc.stdout.decode('utf-8')
+    if not compileError:
+        print("Finish compilation [{:}] to [{:}.py]".format(filename, filename))
+
+        # Run the compiled code.
+        try:
+            proc = subprocess.run(["python", filename + ".py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        except subprocess.CalledProcessError as error:
+            runningError = True
+            outputText = str(error.stderr)
+            print("Error in running Coconut's code")
+
+        if not runningError:
+            # Store output from the running 
+            outputText = proc.stdout.decode('utf-8')
+            print("Finish running [{:}]".format(filename + ".py"))
+
+        # Remove temporary file that we have compiled (*.py)
+        subprocess.run(["rm", filename + '.py'])
+
+    # Remove temporary file that we stored the code 
+    subprocess.run(["rm", filename])
+    print("Delete temp files {:} and {:}.py".format(filename, filename))
+    
+    # Store output in session for showing in browser
+    session['output'] = outputText
     print("Output is\n{:}".format(outputText))
 
-    subprocess.run(["rm", filename])
-    subprocess.run(["rm", filename + '.py'])
-    print("Delete temp fiels {:} and {:}.py".format(filename, filename))
-
-    session['output'] = outputText
-
+    # Redirect to index with POST request (code=307)
     return redirect(url_for('index'), code=307)
 
 if __name__ == '__main__':
