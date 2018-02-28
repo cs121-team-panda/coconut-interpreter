eeddf0ff0fa6adc25f28c6033dc969289797311a
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-02-27 18:27:47 -0800

    tests: setup spy on run request action

--- src/components/__tests__/CodeEditor.test.js
+++ src/components/__tests__/CodeEditor.test.js
@@ -6,36 +6,42 @@ import AceEditor from 'react-ace';
 import CodeEditor from '../CodeEditor';
 import styles from '../CodeEditor.module.css';
 
+let runRequest;
+
+beforeEach(() => {
+  runRequest = jest.fn();
+});
+
 it('renders without crashing', () => {
-  mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  mount(<CodeEditor runRequest={runRequest} loading={false} />);
 });
 
 it('renders one <div.editor /> component', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={false} />);
   expect(wrapper.find(`div.${styles.editor}`)).toHaveLength(1);
 });
 
 it('renders one <div.header /> component', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={false} />);
   expect(wrapper.find(`div.${styles.header}`)).toHaveLength(1);
 });
 
 it('renders one <AceEditor /> component', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={false} />);
   expect(wrapper.find(AceEditor)).toHaveLength(1);
 });
 
 it('renders one <button /> component', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={false} />);
   expect(wrapper.find('button')).toHaveLength(1);
 });
 
 it('renders active run button while not loading', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={false} />);
   expect(wrapper.find('button').instance().disabled).toEqual(false);
 });
 
 it('renders disabled run button while loading', () => {
-  const wrapper = mount(<CodeEditor runRequest={jest.fn()} loading={true} />);
+  const wrapper = mount(<CodeEditor runRequest={runRequest} loading={true} />);
   expect(wrapper.find('button').instance().disabled).toEqual(true);
 });
