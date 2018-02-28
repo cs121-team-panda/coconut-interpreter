fe7ca9b34aa0dbb7801ccfb20b396a657eea1b4a
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-02-27 17:53:25 -0800

    tests: pass in correct props for CodeOutput component

--- src/components/__tests__/CodeOutput.test.js
+++ src/components/__tests__/CodeOutput.test.js
@@ -7,20 +7,20 @@ import CodeOutput from '../CodeOutput';
 import styles from '../CodeOutput.module.css';
 
 it('renders without crashing', () => {
-  mount(<CodeOutput runRequest={jest.fn()} loading={false} />);
+  mount(<CodeOutput value={''} />);
 });
 
 it('renders one <div.output /> component', () => {
-  const wrapper = mount(<CodeOutput runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeOutput value="" />);
   expect(wrapper.find(`div.${styles.output}`)).toHaveLength(1);
 });
 
 it('renders one <div.header /> component', () => {
-  const wrapper = mount(<CodeOutput runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeOutput value="" />);
   expect(wrapper.find(`div.${styles.header}`)).toHaveLength(1);
 });
 
 it('renders one <AceEditor /> component', () => {
-  const wrapper = mount(<CodeOutput runRequest={jest.fn()} loading={false} />);
+  const wrapper = mount(<CodeOutput value="" />);
   expect(wrapper.find(AceEditor)).toHaveLength(1);
 });
