c31f6d9b076823d3d8684ff136edbfb81197c5da
Brittany <bw943600@gmail.com>
2018-03-09 17:34:40 -0800

    fixed/confirmed all builtins, keywords, magic methods are correct
    * addressed minor changes, still fixing new operators
    
    * incorporated new operators up to unicode alternatives
    
    * put all of new operators in a const variable for consistency
    
    * addressed review comments, removed comments and |
    
    * removed trailing | in operators
    
    * removed trailing | in builtins

--- src/utils/coconut.js
+++ src/utils/coconut.js
@@ -9,15 +9,12 @@ export class CoconutHighlightRules extends window.ace.acequire(
     const keywords =
       'and|as|assert|break|class|continue|def|del|elif|else|except|exec|' +
       'finally|for|from|global|if|import|in|is|lambda|not|or|pass|print|' +
-      'raise|return|try|while|with|yield|async|await|data|' +
+      'raise|return|try|while|with|yield|async|await|' +
       // Coconut-specific:
-      'match|case';
-    // TODO: Add rest
+      'match|case|data';
 
     const builtinConstants =
-      'True|False|None|NotImplemented|Ellipsis|__debug__|';
-    // Coconut-specific:
-    // TODO: Add Coconut-specific builtin constants (if applicable?)
+      'True|False|None|NotImplemented|Ellipsis|__debug__';
 
     const builtinFunctions =
       'abs|divmod|input|open|staticmethod|all|enumerate|int|ord|str|any|' +
@@ -29,20 +26,27 @@ export class CoconutHighlightRules extends window.ace.acequire(
       '__import__|complex|hash|min|set|apply|delattr|help|next|setattr|' +
       'buffer|dict|hex|object|slice|coerce|dir|id|oct|sorted|intern|self|' +
       // Coconut-specific
-      'reduce|takewhile|dropwhile|tee|count|makedata|consume|parallel_map|' +
+      'takewhile|dropwhile|tee|count|makedata|consume|parallel_map|' +
       'addpattern|recursive_iterator|concurrent_map|fmap|__fmap__|starmap|reiterable|' +
-      'scan|groupsof|py_chr|py_filter|py_hex|py_input|py_init|py_object|py_oct|' +
-      'py_open|py_print|py_range|py_xrange|py_str|py_map|py_zip|';
+      'scan|groupsof|py_chr|py_filter|py_hex|py_input|py_int|py_object|py_oct|' +
+      'py_open|py_print|py_range|py_xrange|py_str|py_map|py_zip';
 
     const keywordMapper = this.createKeywordMapper(
       {
         'invalid.deprecated': 'debugger',
         'support.function': builtinFunctions,
+        'variable.language': 'self|cls',
         'constant.language': builtinConstants,
         keyword: keywords,
       },
       'identifier'
     );
+
+    const operators =
+      '\\+|\\-|\\*|\\*\\*|\\/|\\/\\/|%|<<|>>|&|\\||\\^|~|<|>|<=|=>|==|!=|<>|=|' +
+      //Coconut Specific:
+      '@|\\$|`|\\|>|(?:<\\*?)?(?!\\.\\.\\.)\\.\\.(?:\\*?>)?|\\|>|<\\||\\|\\*>|' +
+      '<\\*\\||->|\\?\\??';
 
     const pythonHighlightRules = this.getRules();
     pythonHighlightRules.start = pythonHighlightRules.start.filter(
@@ -61,8 +65,7 @@ export class CoconutHighlightRules extends window.ace.acequire(
         },
         {
           token: 'keyword.operator',
-          regex:
-            '\\+|\\-|\\*|\\*\\*|\\/|\\/\\/|%|<<|>>|&|\\||\\^|~|<|>|<=|=>|==|!=|<>|=',
+          regex: operators,
         },
       ],
     };
