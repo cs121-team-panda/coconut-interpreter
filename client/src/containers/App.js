8f07247552af71ae52cc38e8bf8cf66f5a744e7e
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-04-12 23:09:28 -0700

    Use static layout for embed build

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,18 +6,25 @@ import { withStyles } from 'material-ui/styles';
 import EditorContainer from './EditorContainer';
 import OutputContainer from './OutputContainer';
 
+const embed = process.env.REACT_APP_EMBED;
+
 const styles = () => ({
   container: {
     height: '100vh',
+    overflow: 'hidden',
     fontFamily: 'Roboto',
     display: 'grid',
     gridTemplateRows: '1fr 1fr',
     gridTemplateAreas: "'editor' 'output'",
   },
   '@media only screen and (min-width: 600px)': {
-    container: {
-      display: 'flex',
-    },
+    ...(embed && {
+      container: {
+        gridTemplateColumns: '1fr 1fr',
+        gridTemplateRows: '1fr',
+        gridTemplateAreas: "'editor output'",
+      },
+    }),
   },
 });
 
