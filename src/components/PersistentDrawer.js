c5417f818603a1db3385c606b0079488ec082bb5
brittanywang <35832643+brittanywang@users.noreply.github.com>
2018-04-14 18:41:47 -0700

    ⌘+Enter shows up now!  (#40)
    * included toolkit, not showing up though
    
    * fixed syntax errors, tooltip for run works
    
    * accidentally delted settings tooltip
    
    * changed placement

--- src/components/PersistentDrawer.js
+++ src/components/PersistentDrawer.js
@@ -364,7 +364,16 @@ class PersistentDrawer extends React.Component<Props, State> {
               >
                 <SaveIcon className={classes.downloadIcon} />
               </IconButton>
+              <ReactTooltip
+                id="run"
+                className={classes.tooltip}
+                place="bottom"
+                type="dark"
+                effect="solid"
+              />
               <Button
+                data-tip="⌘ + Enter"
+                data-for="run"
                 color="inherit"
                 className={classes.runButton}
                 onClick={this.props.handleClick}
